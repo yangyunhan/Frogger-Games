329f610acd58560324ddbb6123a60a8d6f2265bb
yangyunhan <1205385190@qq.com>
2018-03-25 11:41:58 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
 
-// 这是我们的玩家要躲避的敌人
+// 玩家要躲避的敌人
 var Enemy = function(x,y,speed) {
-    // 要应用到每个敌人的实例的变量写在这里
-    // 我们已经提供了一个来帮助你实现更多
     this.x = x;
     this.y = y;
     this.speed = speed;
-    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
 };
 
-// 此为游戏必须的函数，用来更新敌人的位置
+// 更新敌人的位置
 // 参数: dt ，表示时间间隙
 Enemy.prototype.update = function(dt) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    // 给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
     this.x += dt * this.speed;
     if (this.x>=505)this.x = -50;
@@ -22,7 +19,7 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-// 此为游戏必须的函数，用来在屏幕上画出敌人，
+// 在屏幕上画出敌人，
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -39,8 +36,7 @@ Enemy.prototype.checkCollision = function (player) {
 };
 
 
-// 现在实现你自己的玩家类
-// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+// 玩家类
 var Player = function (x,y) {
     this.x = x;
     this.y = y;
@@ -85,9 +81,6 @@ Player.prototype.handleInput = function (movement) {
             this.y += 83; break;
     }
 };
-// 现在实例化你的所有对象
-// 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
-// 把玩家对象放进一个叫 player 的变量里面
 
 var enemy1 = new Enemy(0,83*2+55,150);
 var enemy2 = new Enemy(101,83*1+55,50);
@@ -116,10 +109,6 @@ playersUl.click(function (e) {
     player.render();
 });
 
-
-
-// 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
-// 方法里面。你不需要再更改这段代码了。
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
